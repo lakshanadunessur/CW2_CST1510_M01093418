2c4d468b6b5cb44c9a78d22a41c048c719084da8
lakshanadunessur <lakshanadunessur@gmail.com>
2025-11-26 22:15:01 +0400

    week9

--- app/data/schema.py
+++ app/data/schema.py
@@ -10,6 +10,65 @@ def create_users_table(conn):
         )
     """)
     conn.commit()
+def create_cyber_incidents_table(conn):
+  cursor = conn.cursor()
+  create_table_sql = """
+  CREATE TABLE IF NOT EXISTS cyber_incidents (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT NOT NULL,
+    incident_type TEXT NOT NULL,
+    severity TEXT NOT NULL,
+    status TEXT NOT NULL,
+    description TEXT NOT NULL,
+    reported_by TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """
+  cursor.execute(create_table_sql)
+  conn.commit()
+  print("Cyber incidents table created successfully!")
+  pass
+
+def create_datasets_metadata_table(conn):
+    cursor = conn.cursor()
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS datasets_metadata (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      dataset_name TEXT NOT NULL,
+      category TEXT NOT NULL,
+      source TEXT NOT NULL,
+      last_updated TEXT NOT NULL,
+      record_count INTEGER NOT NULL,
+      file_size_mb REAL NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+      """
+    cursor.execute(create_table_sql)
+    conn.commit()
+    print("Datasets metadata table created successfully!")
+    pass
+
+def create_it_tickets_table(conn):
+      cursor = conn.cursor()
+      create_table_sql = """
+      CREATE TABLE IF NOT EXISTS it_tickets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ticket_id TEXT NOT NULL UNIQUE,
+        priority TEXT NOT NULL,
+        status TEXT NOT NULL,
+        category TEXT NOT NULL,
+        subject TEXT NOT NULL,
+        description TEXT
+        created_date TEXT
+        resolved_date TEXT,
+        assigned_to TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+      cursor.execute(create_table_sql)
+      conn.commit()
+      print("IT tickets table created successfully!")
+      pass
 
 def create_all_tables(conn):
     """Create all tables."""
